db6c29795c63f4d0150ff30a7f2bf41e9a69ff31
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2023-09-26 07:04:13 -0300

    Cambios a ser confirmados: 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/__init__.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/__manifest__.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/__pycache__/__init__.cpython-38.pyc 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/controllers/__init__.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/controllers/__pycache__/__init__.cpython-38.pyc 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/controllers/__pycache__/controllers.cpython-38.pyc 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/controllers/controllers.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/models/__init__.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/models/__pycache__/__init__.cpython-38.pyc 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/models/__pycache__/models.cpython-38.pyc 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/models/models.py 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/views/templates.xml 	nuevos archivos: pghiglione_change_name_street2_to_between_streets/views/views.xml 	modificados:     pghiglione_fixes_deltatech_website_delivery_and_payment/__manifest__.py

---
+++ pghiglione_change_name_street2_to_between_streets/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "pghiglione_change_name_street2_to_between_streets",
+    "category": "Website",
+    "summary": "Cambia el nombre calle 2 por Entre calles",
+    "version": "15.0.2.1.0",
+    "author": "Ghiglione Pedro Matias",
+    "license": "OPL-1",
+    "website": "https://quimerasoftware.ar",
+    "depends": ["website_sale"],
+    "data": [
+        "views/delivery_view.xml",
+    ],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["Ghiglione Pedro Matias"],
+}
